fe698b1ea22b47207a4e3813889d1f3c084c7115
Shayanzahid227 <sshayanzahid227@gmail.com>
2025-02-06 22:13:21 +0500

    logout user if he want and navigate to login screen again

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:code_structure/core/constants/colors.dart';
+import 'package:code_structure/ui/screens/authentication/log/login_screen.dart';
+import 'package:code_structure/ui/screens/authentication/utils/sign_in_error_message.dart';
 import 'package:code_structure/ui/screens/home/home_screen_viev_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _auth = FirebaseAuth.instance;
+
   int counter = 0;
   int _counter() {
     return counter = counter + 1;
@@ -22,29 +28,61 @@ class _HomeScreenState extends State<HomeScreen> {
       create: (context) => HomeViewModel(),
       child: Consumer<HomeViewModel>(
           builder: (context, model, child) => Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.purple.shade200,
+                  actions: [
+                    IconButton(
+                        onPressed: () {
+                          _auth.signOut().then(
+                            (value) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LogInScreen(),
+                                ),
+                              );
+                            },
+                          ).onError(
+                            (error, stackTrace) {
+                              Utils().ToastMessage(
+                                error.toString(),
+                              );
+                            },
+                          );
+                        },
+                        icon: Icon(Icons.logout)),
+                    30.horizontalSpace
+                  ],
+                ),
                 body: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Center(
                       child: GestureDetector(
                         onTap: () {},
-                        child: Text("shayan zahid $counter"),
+                        // child: Text("shayan zahid $counter"),
+                        child: Text(""),
                       ),
                     ),
                     GestureDetector(
                       onTap: () {
                         setState(() {
-                          _counter();
+                          // _counter();
                         });
                       },
                       child: Container(
-                        height: 70,
-                        width: 120,
-                        color: Colors.deepPurple,
+                        height: 60,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(30)),
                         child: Center(
                             child: Text(
-                          "tap me ",
-                          style: TextStyle(color: whiteColor),
+                          "welcome back ",
+                          style: TextStyle(
+                              color: whiteColor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 25),
                         )),
                       ),
                     )
